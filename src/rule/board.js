9e77bd60cd5f37f5b6591e71af9ac746db7b37e3
tringcooler <tringcooler@users.noreply.github.com>
2020-01-21 19:36:55 +0800

    Update board.js

--- src/rule/board.js
+++ src/rule/board.js
@@ -1,12 +1,27 @@
 define(function(require) {
     
+    const
+        c_deck = require('core/deck');
+    
     const [
-        
+        PL_DECKS,
+        MTD_INIT_DECKS,
     ] = require('core/util').symgen();
     
     class c_board {
         
-        constructor() {
+        constructor(draw_deck) {
+            this[PL_DECKS] = {};
+            this[MTD_INIT_DECKS]();
+        }
+        
+        [MTD_INIT_DECKS](draw_deck) {
+            this[PL_DECKS].draw = draw_deck;
+            this[PL_DECKS].hand = new c_deck();
+            this[PL_DECKS].drop = new c_deck();
+            this[PL_DECKS].elim = new c_deck();
+            this[PL_DECKS].dun = new c_deck();
+            this[PL_DECKS].bag = new c_deck();
         }
         
     }
