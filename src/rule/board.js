8d3271a306ae99c661f4c5b5c05e0701d55624fd
tringcooler <tringcooler@users.noreply.github.com>
2020-01-21 18:32:47 +0800

    Create board.js

---
+++ src/rule/board.js
@@ -0,0 +1,16 @@
+define(function(require) {
+    
+    const [
+        
+    ] = require('core/util').symgen();
+    
+    class c_board {
+        
+        constructor() {
+        }
+        
+    }
+    
+    return c_board;
+    
+});
