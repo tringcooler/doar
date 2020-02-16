d00cc950078652e0e365ccb9e45582e7b8f422f3
tringcooler <tringcooler@users.noreply.github.com>
2020-02-17 05:28:01 +0800

    Update tagtab.js

--- src/core/tagtab.js
+++ src/core/tagtab.js
@@ -10,6 +10,7 @@ define(function(require) {
         MTD_PREV, MTD_AFTER,
         
         // for tag
+        PR_TAB,
         MTD_COMPILE,
         
         // for tag tab
@@ -39,7 +40,13 @@ define(function(require) {
         
     }
     
-    function _bs2tr(src, tr, trstk) {
+    const [
+        TS_SEP, TS_INV,
+    ] = [
+        ':', '$',
+    ];
+    
+    function _bs2tr(src, tr = [], trstk = []) {
         let bui = src.indexOf('(');
         let bdi = src.indexOf(')');
         if(bdi >= 0 && (bdi < bui || bui < 0)) {
@@ -67,18 +74,64 @@ define(function(require) {
         }
         return tr;
     }
-    bs2tr = _bs2tr;
-    bstst = ["aaaa(bbb(c))ddd((ee()fff)ggg)", [], []]
-    console.log(_bs2tr(...bstst));
-    console.log(bstst);
+    
+    function _parse_btr(tr) {
+        let rtr = [];
+        for(let e of tr) {
+            let lst_r = rtr[rtr.length - 1];
+            if(typeof(e) == 'string') {
+                let er = e.split(TS_SEP);
+                for(let i = er.length - 1; i >= 0 ; i--) {
+                    er[i] = er[i].trim();
+                    if(i == 0 && lst_r instanceof Array) {
+                        if(lst_r.length <= 0) {
+                            rtr.pop();
+                        }
+                        if(!er[i]) {
+                            er.shift();
+                        } else {
+                            return null;
+                        }
+                    } else if(i < er.length - 1 && !er[i]) {
+                        return null;
+                    }
+                }
+                rtr = rtr.concat(er);
+            } else {
+                if(!lst_r) {
+                    rtr.pop();
+                } else {
+                    return null;
+                }
+                let nr = _parse_btr(e);
+                if(nr === null) {
+                    return null;
+                }
+                rtr.push(nr);
+            }
+        }
+        if(!rtr[rtr.length - 1]) {
+            if(rtr.length > 1) {
+                return null;
+            } else {
+                rtr.pop();
+            }
+        }
+        return rtr;
+    }
+    
+    console.log(_parse_btr(_bs2tr("aaaa:(bbb:(c)):ddd:((ee:():fff):ggg)")));
     
     class c_tag {
         
-        constructor(src) {
+        constructor(src, tab) {
+            this[PR_TAB] = tab;
         }
         
         [MTD_COMPILE](src) {
-            
+            let btr = _bs2tr(src);
+            if(btr === null) return null;
+            let rtr = _parse_btr(btr);
         }
         
     }
